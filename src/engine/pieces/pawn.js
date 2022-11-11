f6ccdafa6ef87eb8ccec9635811725fbb784a5a2
Muh90 <muhanad.al-khafaji@softwire.com>
2022-11-11 14:13:41 +0000

    Allow pawns to move 1 or 2 steps forward if in starting position.

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -7,12 +7,24 @@ export default class Pawn extends Piece {
     }
 
     getAvailableMoves(board) {
-        const currentSquare = board.findPiece(this);
+        let currentSquare = board.findPiece(this);
         let direction = 1;
+        let startingRow = 1;
         if (this.player === Player.BLACK) {
             direction = -1;
+            startingRow = 6;
         }
-        currentSquare.row += direction;
-        return [currentSquare];
+        const availableSquares = []
+        let newSquare;
+        if (currentSquare.row === startingRow) {
+            newSquare = currentSquare;
+            newSquare.row += 2 * direction;
+            availableSquares.push(newSquare);
+            currentSquare = board.findPiece(this);
+        }
+        newSquare = currentSquare;
+        newSquare.row += direction;
+        availableSquares.push(newSquare);
+        return availableSquares;
     }
 }
