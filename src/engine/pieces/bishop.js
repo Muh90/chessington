242172eea1b0c98ebf528e37edaff2236ce0186f
Muh90 <muhanad.al-khafaji@softwire.com>
2022-11-11 15:05:35 +0000

    Added bishop movement in diagonals on the board.

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import GameSettings from "../gameSettings";
 
 export default class Bishop extends Piece {
     constructor(player) {
@@ -6,6 +7,24 @@ export default class Bishop extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let currentSquare = board.findPiece(this);
+        const availableSquares = [];
+        for (let row = 0; row < GameSettings.BOARD_SIZE; row++) {
+            if (row !== currentSquare.row) {
+                let newSquare = Object.create(currentSquare);
+                newSquare.col = currentSquare.col - currentSquare.row + row;
+                if (newSquare.col >= 0 && newSquare.col < 8) {
+                    newSquare.row = row;
+                    availableSquares.push(newSquare);
+                }
+                newSquare = Object.create(currentSquare);
+                newSquare.col = currentSquare.col + currentSquare.row - row;
+                if (newSquare.col >= 0 && newSquare.col < 8) {
+                    newSquare.row = row;
+                    availableSquares.push(newSquare);
+                }
+            }
+        }
+        return availableSquares;
     }
 }
