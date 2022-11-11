3cc256aabc6ab663aa5d9610a444ab8ce7bbae25
Muh90 <muhanad.al-khafaji@softwire.com>
2022-11-11 15:48:30 +0000

    Added king movement on the board.

--- src/engine/pieces/king.js
+++ src/engine/pieces/king.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import GameSettings from "../gameSettings";
 
 export default class King extends Piece {
     constructor(player) {
@@ -6,6 +7,21 @@ export default class King extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let currentSquare = board.findPiece(this);
+        const availableSquares = [];
+        const distances = [-1, 0, 1];
+        for (let i = 0; i < distances.length; i++) {
+            for (let j = 0; j < distances.length; j++) {
+                if (i !== 1 || j !== 1) {
+                    let newSquare = Object.create(currentSquare);
+                    newSquare.row = currentSquare.row + distances[i];
+                    newSquare.col = currentSquare.col + distances[j];
+                    if (newSquare.row >= 0 && newSquare.row < 8 && newSquare.col >= 0 && newSquare.col < 8) {
+                        availableSquares.push(newSquare);
+                    }
+                }
+            }
+        }
+        return availableSquares;
     }
 }
