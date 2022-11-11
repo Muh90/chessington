51abb5d8d95d0b5e8de5ba2d6889a7bea654b499
Muh90 <muhanad.al-khafaji@softwire.com>
2022-11-11 13:00:23 +0000

    Adjusted pawns to be able to move 1 step towards opponents' side during a player's turn.

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Player from "../player";
 
 export default class Pawn extends Piece {
     constructor(player) {
@@ -6,6 +7,12 @@ export default class Pawn extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        const currentSquare = board.findPiece(this);
+        let direction = 1;
+        if (this.player === Player.BLACK) {
+            direction = -1;
+        }
+        currentSquare.row += direction;
+        return [currentSquare];
     }
 }
