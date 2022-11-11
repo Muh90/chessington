62e9cb735a6c47316a260a51a9489f2aa3d9a5ad
Muh90 <muhanad.al-khafaji@softwire.com>
2022-11-11 15:27:26 +0000

    Added knight movement on the board.

--- src/engine/pieces/knight.js
+++ src/engine/pieces/knight.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import GameSettings from "../gameSettings";
 
 export default class Knight extends Piece {
     constructor(player) {
@@ -6,6 +7,23 @@ export default class Knight extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let currentSquare = board.findPiece(this);
+        const availableSquares = [];
+        const distances = [-2, -1, 1, 2];
+        for (let i = 0; i < distances.length; i++) {
+            let newSquare = Object.create(currentSquare);
+            newSquare.row = currentSquare.row + distances[i]
+            newSquare.col = currentSquare.col + (Math.abs(distances[i]) % 2 + 1);
+            if (newSquare.row >= 0 && newSquare.row < 8 && newSquare.col >= 0 && newSquare.col < 8) {
+                availableSquares.push(newSquare);
+            }
+            newSquare = Object.create(currentSquare);
+            newSquare.row = currentSquare.row + distances[i]
+            newSquare.col = currentSquare.col - (Math.abs(distances[i]) % 2 + 1);
+            if (newSquare.row >= 0 && newSquare.row < 8 && newSquare.col >= 0 && newSquare.col < 8) {
+                availableSquares.push(newSquare);
+            }
+        }
+        return availableSquares;
     }
 }
